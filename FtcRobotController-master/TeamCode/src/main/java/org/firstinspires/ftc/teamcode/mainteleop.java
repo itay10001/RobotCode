1815ba8b8dbe4c2d42df206eb37e7f57fb914ed9
itay10001 <157873702+itay10001@users.noreply.github.com>
2025-12-10 12:05:54 +0200

    servo, failsafe, and more
    i added the wall servo to the code and added a failsafe to the colorsensors so that if the front color sensor doesnt work then the back one will check if it has a ball so then it will decide that there are 3 balls in the holding and made it so that the value of the colorsensors will be shown this can be removed before the competition but would be nice to have

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainteleop.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainteleop.java
@@ -1,79 +1,156 @@
+package org.firstinspires.ftc.teamcode;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.IMU;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp
+@TeleOp(name = "mainteleop")
 public class mainteleop extends LinearOpMode {
+
+    private DcMotor transportMotor;
+    private DcMotor shootingMotor;
+    private DcMotor intakeMotor;
+
+    private ColorSensor color1;
+    private ColorSensor color2;
+    private ColorSensor color3;
+    private ColorSensor color4;
+
+    private Servo gateServo;
+
+    private static final int BALL_THRESHOLD = 500;
+    private static final double GATE_CLOSED = 0.0;
+    private static final double GATE_OPEN   = 0.7;
+
+    public enum RobotState {
+        IDLE,
+        INTAKE,
+        HOLDING,
+        SHOOTING
+    }
+
+    private RobotState currentState = RobotState.IDLE;
+
+    @Override
     public void runOpMode() throws InterruptedException {
-        Dcmotor TransportMotor = hardwaremap.dcMotor.get("HM");
-        Dcmotor ShootingMotor = hardwaremap.dcMotor.get("SM");
-        Dcmotor IntakeMotor = hardwaremap.dcMotor.get("INTM");
-        ColorSensor color1 = hardwareMap.get(ColorSensor.class, "color1");
-        ColorSensor color2 = hardwareMap.get(ColorSensor.class, "color2");
-        ColorSensor color3 = hardwareMap.get(ColorSensor.class, "color3");
-        ColorSensor color4 = hardwareMap.get(ColorSensor.class, "color4");
 
+        transportMotor = hardwareMap.get(DcMotor.class, "HM");
+        shootingMotor  = hardwareMap.get(DcMotor.class, "SM");
+        intakeMotor    = hardwareMap.get(DcMotor.class, "INTM");
 
-        Public boolean isBallFront() {
-            Return color1.alphe() > 500 || color2.alphe() > 500;
-        }
+        color1 = hardwareMap.get(ColorSensor.class, "color1");
+        color2 = hardwareMap.get(ColorSensor.class, "color2");
+        color3 = hardwareMap.get(ColorSensor.class, "color3");
+        color4 = hardwareMap.get(ColorSensor.class, "color4");
 
-        Public boolean isBallRear() {
-            Return color3.alphe() > 500 || color4.alphe() > 500;
-        }
-        public enum RobotState {
-            IDLE,
-            HOLDING,
-            INTAKE,
-            SHOOTING
-        }
-        Public int getNumberOfBalls() {
-            If(!isBallFront() && !isBallRear()) {
-                Return 0;
-            } else if (isBallFront() && !isBallRear()) {
-                Return 1;
-            } else if (isBallFront() && isBallRear()) {
-                Return 3;
+        gateServo = hardwareMap.get(Servo.class, "gateServo");
+        gateServo.setPosition(GATE_CLOSED);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            int ballCount = getNumberOfBalls();
+            boolean hasBalls  = ballCount > 0;
+            boolean full      = ballCount == 3;
+            boolean lessThan3 = ballCount < 3;
+
+            boolean button1 = gamepad1.x;
+            boolean button2 = gamepad1.a;
+            boolean button3 = gamepad1.y;
+
+            if (hasBalls && button2) {
+                currentState = RobotState.SHOOTING;
             }
-        }
+            else if (lessThan3 && button1) {
+                currentState = RobotState.INTAKE;
+            }
+            else if ((full && currentState == RobotState.INTAKE) ||
+                     (button3 && hasBalls)) {
+                currentState = RobotState.HOLDING;
+            }
+            else if ((!hasBalls && currentState == RobotState.SHOOTING) ||
+                     (button3 && !hasBalls)) {
+                currentState = RobotState.IDLE;
+            }
 
-        switch (currentState) {
+            setStateOutputs(currentState);
+
+            telemetry.addData("State", currentState);
+            telemetry.addData("Balls", ballCount);
+
+
+            telemetry.addData("color1 alpha", color1.alpha());
+            telemetry.addData("color2 alpha", color2.alpha());
+            telemetry.addData("color3 alpha", color3.alpha());
+            telemetry.addData("color4 alpha", color4.alpha());
+
+            telemetry.update();
+        }
+    }
+
+    private boolean isBallFront() {
+        return (color1.alpha() > BALL_THRESHOLD) ||
+               (color2.alpha() > BALL_THRESHOLD);
+    }
+
+    private boolean isBallRear() {
+        return (color3.alpha() > BALL_THRESHOLD) ||
+               (color4.alpha() > BALL_THRESHOLD);
+    }
+
+    private int getNumberOfBalls() {
+        boolean front = isBallFront();
+        boolean rear  = isBallRear();
+
+        if (!front && !rear) {
+            return 0;
+        } else if (front && !rear) {
+            return 1;
+        } else if (front && rear) {
+            return 3;
+        } else if (!front && rear) {
+            return 3;
+        }
+    }
+
+    private void setStateOutputs(RobotState state) {
+        switch (state) {
+
             case IDLE:
-                IntakeMotor.setPower(0);
-                TransportMotor.setPower(0)
-                ShootingMotor.setPower(0);
+                gateServo.setPosition(GATE_CLOSED);
+                intakeMotor.setPower(0.0);
+                transportMotor.setPower(0.0);
+                shootingMotor.setPower(0.0);
                 break;
 
             case INTAKE:
-                IntakeMotor.setPower(1.0);
-                TransportMotor.setPower(1.0)
-                ShootingMotor.setPower(0);
+                gateServo.setPosition(GATE_CLOSED);
+                intakeMotor.setPower(1.0);
+                transportMotor.setPower(1.0);
+                shootingMotor.setPower(0.0);
                 break;
 
             case HOLDING:
-                IntakeMotor.setPower(0)
-                TransportMotor.setPower(0)
-                ShootingMotor.setPower(1.0);
+                gateServo.setPosition(GATE_CLOSED);
+                intakeMotor.setPower(0.0);
+                transportMotor.setPower(0.0);
+                shootingMotor.setPower(1.0);
+                break;
+
             case SHOOTING:
-                IntakeMotor.setPower(0);
-                TransportMotor.setPower(1.0)
-                ShootingMotor.setPower(1.0);
+                gateServo.setPosition(GATE_OPEN);
+                intakeMotor.setPower(0.0);
+                transportMotor.setPower(1.0);
+                shootingMotor.setPower(1.0);
                 break;
         }
-        currentState = RobotState.IDLE;
-        if (getNumberOfBalls == 0 && gamepad1.x = true):
-        currentState = RobotState.INTAKE;
-        else if (getNumberOfBalls == 3 && gamepad1.y = true):
-        currentState = RobotState.HOLDING;
-        else if (gamepad1.a = true):
-        currentState = RobotState.SHOOTING;
-        else if (getNumberOfBalls == 1):
-        currentState = RobotState.IDLE;
-
-
     }
 }
+telemetry.addData("color1 alpha", color1.alpha());
+telemetry.addData("color2 alpha", color2.alpha());
+telemetry.addData("color3 alpha", color3.alpha());
+telemetry.addData("color4 alpha", color4.alpha());
+telemetry.update();
