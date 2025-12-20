a75ac10d9092317e5bd2d1ccd4b5f9b5bcba6d32
itay10001 <157873702+itay10001@users.noreply.github.com>
2025-12-20 19:47:08 +0200

    added autonomous

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanomDrive.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanomDrive.java
@@ -6,24 +6,39 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.Servo;
+
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @TeleOp
 public class MechanomDrive extends LinearOpMode {
+
+    double gatePosition = -0.1;
+
     @Override
     public void runOpMode() throws InterruptedException {
         DcMotor FLM = hardwareMap.dcMotor.get("FLM");
         DcMotor BLM = hardwareMap.dcMotor.get("BLM");
         DcMotor FRM = hardwareMap.dcMotor.get("FRM");
         DcMotor BRM = hardwareMap.dcMotor.get("BRM");
+        //Servo gate = hardwareMap.servo.get("gate");
+
+        FLM.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BLM.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        FRM.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BRM.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        DcMotor m1 = hardwareMap.dcMotor.get("m1");
+        DcMotor m2 = hardwareMap.dcMotor.get("m2");
+        DcMotor m3 = hardwareMap.dcMotor.get("m3");
 
         IMU imu = hardwareMap.get(IMU.class, "imu");
 // Adjust the orientation parameters to match your robot
         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+                RevHubOrientationOnRobot.LogoFacingDirection.DOWN,
+                RevHubOrientationOnRobot.UsbFacingDirection.LEFT));
 // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
-        imu.initialize(parameters);
+        imu.initialize( parameters);
 
         FRM.setDirection(DcMotorSimple.Direction.REVERSE);
         BRM.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -31,12 +46,42 @@ public class MechanomDrive extends LinearOpMode {
         waitForStart();
 
 
-
         while (opModeIsActive()) {
-            double y = -gamepad1.left_stick_y * 0.5; // Remember, Y stick value is reversed
-            double x = gamepad1.left_stick_x * 0.5;
-            double rx = -gamepad1.right_stick_x * 0.5;
+            double y = -gamepad1.left_stick_y ; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x ;
+            double rx = -gamepad1.right_stick_x;
 
+
+
+            double power = 0;
+
+            if (gamepad1.right_trigger > 0.5){
+                y = y *0.5;
+                x = x*0.5;
+                rx = rx*0.5;
+                m1.setPower(-1);
+            }   else {
+
+                m1.setPower(0);
+            }
+
+            if (gamepad2.right_trigger > 0.5) {
+                power = -1;
+                m2.setPower(-0.73);
+            }   else{
+                m2.setPower(-0.60);
+            }
+
+            if (gamepad2.left_trigger > 0.5) {
+                power = -1;
+
+            }
+
+            m3.setPower(power);
+
+
+
+            //galdo sybau
             // This button choice was made so that it is hard to hit on accident,
             // it can be freely changed based on preference.
             // The equivalent button is start on Xbox-style controllers.
@@ -61,11 +106,16 @@ public class MechanomDrive extends LinearOpMode {
             double frontRightPower = (rotY - rotX - rx) / denominator;
             double backRightPower = (rotY + rotX - rx) / denominator;
 
+
+
             FLM.setPower(frontLeftPower);
             BLM.setPower(backLeftPower);
             FRM.setPower(frontRightPower);
             BRM.setPower(backRightPower);
 
+            telemetry.addData("Angle:", -botHeading);
+            telemetry.addData("gateposition: ", gatePosition);
+            telemetry.update();
 
         }
     }
