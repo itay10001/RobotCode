147ccc1b30e8f75cae0235ca20b441373ac52c29
itay10001 <157873702+itay10001@users.noreply.github.com>
2025-12-29 10:04:10 +0200

    Update MechanomDrive.java

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanomDrive.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanomDrive.java
@@ -34,11 +34,10 @@ public class MechanomDrive extends LinearOpMode {
 
         IMU imu = hardwareMap.get(IMU.class, "imu");
 // Adjust the orientation parameters to match your robot
-        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-
-                
-// Without this, the REV Hub's orientation is assumed to be logo up / USB forward
-        imu.initialize( parameters);
+                IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.DOWN,
+                RevHubOrientationOnRobot.UsbFacingDirection.LEFT));
+        imu.initialize(parameters);
 
         FRM.setDirection(DcMotorSimple.Direction.REVERSE);
         BRM.setDirection(DcMotorSimple.Direction.REVERSE);
